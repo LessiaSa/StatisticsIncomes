c15dfa0d1da9c52be32e8be308edbd8db5a24886
LessiaSa <0162221@mail.ru>
2024-06-22 12:49:13 +0700

    NewTests

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -10,5 +10,39 @@ public class StatisticsService {
         }
         return currentMax;
     }
+    public long findMin(long[] incomes) {
+        long currentMin = incomes[0];
+        for (long income : incomes) {
+            if (currentMin > income) {
+                currentMin = income;
+            }
+        }
+        return currentMin;
+    }
+    public long findSame(long[] incomes) {
+        long currentSame = incomes[0];
+        for (long income : incomes) {
+            if (currentSame == income) {
+                currentSame = income;
+            }
+        }
+        return currentSame;
+    }
+    public long findMedian(long[] incomes) {
+        long currentMedian = incomes[0];
+        for (long income : incomes) {
+            if (currentMedian != income) {
+               currentMedian = income;
+            }
+        }
+        return currentMedian;
+    }
+    public long findSum(long[] incomes) {
+        long currentSum = incomes[0];
+        for (long income : incomes) {
+            currentSum += income;
+        }
+        return currentSum;
 
+    }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,6 +10,42 @@ public class StatisticsServiceTest {
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
         long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12,5,8,4,5,3,8,6,11,11,12};
+        long expected = 3;
+        long actual = service.findMin(incomesInBillions);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void findSame() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12,5,8,4,5,3,8,6,11,11,12};
+        long expected = 12;
+        long actual = service.findSame(incomesInBillions);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void findMedian() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12,5,8,4,5,3,8,6,11,11,12};
+        long expected = 12;
+        long actual = service.findMedian(incomesInBillions);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void findSum() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12,5,8,4,5,3,8,6,11,11,12};
+        long expected = 97;
+        long actual = service.findSum(incomesInBillions);
         Assertions.assertEquals(expected,actual);
     }
 }
